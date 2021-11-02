39b99f98a2fcc391fcf4638fedb0147c00104aff
José Antonio González Morente <jgonzalezmorente@gmail.com>
2021-11-02 11:05:38 +0100

    Fin sección 8

---
+++ helpers/jwt.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+
+const generarJWT = ( uid ) => {
+
+    return new Promise( ( resolve, reject ) => {
+        
+        const payload = { uid };
+        jwt.sign( payload, process.env.JWT_KEY, {
+            expiresIn: '24h'
+        }, ( err, token ) => (err) ? reject( 'No se pudo generar el JWT' ) : resolve( token ) );
+    });
+
+}
+
+
+module.exports = {
+    generarJWT
+}
+
